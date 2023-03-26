52258e97922ff02a5ba35d099cfe7735d97884d6
hraghunath <hraghunath08@gmail.com>
2023-03-26 23:08:51 +0530

    Add New Project

---
+++ src/main/java/com/raghu/App.java
@@ -0,0 +1,15 @@
+package com.raghu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    int i=10 ;
+    int j=20;
+       {
+        System.out.println( "Hello World!" );
+    }
+}
